fa9d9a31ae6b67b168673bc089d29522876e2962
ivan-zhukouski <stigmatix.by@gmail.com>
2020-02-10 12:59:20 +0300

    added fakebackend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import  React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import MainPage from './Components/MainPage'
 
 export default class App extends Component {
   render() {
     return(
         <>
-          <h1>
-            Hello World!
-          </h1>
+          <MainPage/>
         </>
     )
   }
